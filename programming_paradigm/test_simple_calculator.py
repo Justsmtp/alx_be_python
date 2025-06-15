93d62fa8e07860d7107c1a1bf88c91c93e0b02d2
Justsmtp <smtpwtf@gmail.com>
2025-06-15 23:04:57 +0100

    Adding new file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(2.5, 3.5), 6.0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
 
     def test_multiply(self):
